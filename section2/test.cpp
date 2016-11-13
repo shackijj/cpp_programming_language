661807110edabe4900532cd4c65f73efd7622355
k.shatsky <k.shatsky>
2016-11-13 20:58:26 +0300

    Initial

---
+++ section2/test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main() 
+{
+    int a = 2;
+    int b = 2;
+
+    // Const and constexpr errors
+
+    /* 
+    const 
+
+    int count(const vector<int>&);
+    const vector<int> v {1, 2, 3, 4, 5};
+    constexpr int rc = count(v);
+    */
+    cout << rc << "\n";
+
+    if (!(a > b)) {
+        cout << "Hello from if statement\n";
+    }
+
+    return 0;
+}
